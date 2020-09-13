cd09dc09755aad7bc5d495db013d71756fb1d242
yasco <ozluyasin20@gmail.com>
2020-09-13 11:24:54 +0100

    little changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
 def head():
-    return render_template("index.html", number1 = 32, number2 = 84)
+    return render_template("index.html", number1 = 32, number2 = 34)
 
 @app.route("/about")
 def second():
-    return render_template("about.html", hazirlayan = "Yasin OZLU")
+    return render_template("about.html", prepared_by = "Yasin")
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+
+if(__name__) == "__main__":
+    app.run(debug=True)
